6123ca34570f52e8efde03799b7ffc4cf876fb0b
sntwandy <sntwandy@gmail.com>
2020-05-30 22:40:26 -0400

    form finished fix 8

--- send.php
+++ send.php
@@ -1,12 +1,12 @@
 <?php
-    $name = $_POST['nombre'];
+    $nombre = $_POST['nombre'];
     $email = $_POST['email'];
     $header = 'From: ' .$email "\r\n";
     $header .= "X-Mailer: PHP/" . phpversion() . "\r\n";
     $header .= "Mime-Version: 1.0 \r\n";
     $header .= "Content-Type: text/plain";
 
-    $message = "Este mensaje fue enviado por " . $name . "\r\n";
+    $message = "Este mensaje fue enviado por " . $nombre . "\r\n";
     $message .= "Su email es: " . $email . "\r\n";
 
     $para = "sntwandy@gmail.com";
