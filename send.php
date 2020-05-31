f704f5e06a14beff605639b4eaa5bab9986e653d
sntwandy <sntwandy@gmail.com>
2020-05-30 22:23:36 -0400

    form finished fix 3

--- send.php
+++ send.php
@@ -1,18 +1,18 @@
 <?php
-    $nombre = $_POST['nombre'];
+    $name = $_POST['nombre'];
     $email = $_POST['email'];
-    $header = 'From: ' .$email;
+    $header = 'From: ' .$email "\r\n";
     $header .= "X-Mailer: PHP/" . phpversion() . "\r\n";
     $header .= "Mime-Version: 1.0 \r\n";
     $header .= "Content-Type: text/plain";
 
-    $mensaje = "Este mensaje fue enviado por " . $nombre . "\r\n";
-    $mensaje .= "Su email es: " . $email . "\r\n";
+    $message = "Este mensaje fue enviado por " . $name . "\r\n";
+    $message .= "Su email es: " . $email . "\r\n";
 
-    $para = "sntwandy@gmail.com";
+    $para = "20161444@miucateci.edu.do";
     $asunto = "Prueba de correo";
 
-    @mail($para, $asunto, utf8_decode($mensaje), $header);
+    mail($para, $asunto, utf8_decode($message), $header);
 
     echo 'Mensaje enviado correctamente';
 ?>
